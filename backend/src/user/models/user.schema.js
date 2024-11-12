c27083c9a129f3a0fa7d8b066359d0aa9a0a0ebd
vikas-pmt <vikas.ranjan@permi.tech>
2024-11-12 20:56:51 +0530

    updated schema

--- backend/src/user/models/user.schema.js
+++ backend/src/user/models/user.schema.js
@@ -50,11 +50,11 @@ const userSchema = new mongoose.Schema({
   document: {
     public_id: {
       type: String,
-      required: true,
+      // required: true,
     },
     url: {
       type: String,
-      required: true,
+      // required: true,
     },
   },
   role: {
