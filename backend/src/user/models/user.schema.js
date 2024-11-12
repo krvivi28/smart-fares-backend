064d41c95587a3f4cb02b2f13e88195592c58938
vikas-pmt <vikas.ranjan@permi.tech>
2024-11-12 16:48:52 +0530

    fixed env fixes and env variables

--- backend/src/user/models/user.schema.js
+++ backend/src/user/models/user.schema.js
@@ -65,7 +65,7 @@ userSchema.pre("save", async function (next) {
 
 // JWT Token
 userSchema.methods.getJWTToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_Secret, {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_Expire,
   });
 };
